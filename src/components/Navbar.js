b22c0f2980dfea41544a7e9664aa974f4a5ae5c6
HieuDang3021 <trhieu1247@gmail.com>
2024-11-26 07:05:26 +1100

    responsive navbar(incomplete)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 import {SidebarData} from './SidebarData'
 import potrait from "../assets/images/potrait-image-example.jpg"
 
 function Navbar() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
+    <div className="Navbar">
     <div className='Sidebar'>
       <div href="/">
         <img className='potrait' src={potrait} alt="Potrait" />
@@ -39,6 +47,64 @@ function Navbar() {
           </div>
       </div>
     </div>
+
+    {/* Top bar for mobile */}
+    <div className="TopBar">
+      <img className="potrait" src={potrait} alt="Portrait" />
+      <p className="SidebarText">Hieu Trung Dang</p>
+      <p className="SidebarText">Full-stack developer</p>
+      <button className="MenuButton" onClick={toggleMenu}>
+        ☰
+      </button>
+    </div>
+
+    {/* Dropdown menu */}
+    <div className={`DropdownMenu ${isOpen ? 'show' : ''}`}>
+      <ul className="SidebarList">
+        {SidebarData.pagelink.map((val, key) => {
+          return (
+            <li
+              key={key}
+              className="row"
+              id={window.location.pathname === val.link ? 'active' : ''}
+              onClick={() => {
+                window.location.pathname = val.link;
+              }}
+            >
+              <div id="icon">{val.icon}</div>
+              <div id="title">{val.title}</div>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="contact-container">
+        <p>Contact me</p>
+        <div className="contact-icons">
+          <a
+            href={SidebarData.contactme.linkedinLink || 'https://www.linkedin.com'}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {SidebarData.contactme.linkedinIcon}
+          </a>
+          <a
+            href={SidebarData.contactme.facebookLink || 'https://www.facebook.com'}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {SidebarData.contactme.facebookIcon}
+          </a>
+          <a
+            href={SidebarData.contactme.githubLink || 'https://www.github.com'}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {SidebarData.contactme.githubIcon}
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
   )
 }
 
